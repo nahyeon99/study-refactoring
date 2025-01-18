17c272436e2331e518931568022ba635b7e4ed58
nahyeon99 <nahyeonee99@gmail.com>
2025-01-19 02:26:23 +0900

    feat: htmlStatement 메서드 추가

--- src/main/java/chapter1/Statement.java
+++ src/main/java/chapter1/Statement.java
@@ -10,8 +10,28 @@ import chapter1.dto.StatementData;
 
 public class Statement {
 
+	public String htmlStatement(final Invoice invoice, final Map<String, Play> plays) {
+		return renderHtml(StatementData.from(invoice, plays));
+	}
+
 	public String statement(final Invoice invoice, final Map<String, Play> plays) {
 		return renderPlainText(StatementData.from(invoice, plays));
+	}
+
+	private String renderHtml(final StatementData data) {
+		var result = new StringBuilder(String.format("<h1>청구 내역 (고객명 : %s)</h1>\n", data.customer()));
+		result.append("<table>\n");
+		result.append("  <tr><th>연극</th><th>총액</th><th>좌석 수</th><th>금액</th></tr>\n");
+		for (var performance : data.performances()) {
+			result.append(
+				String.format("  <tr><td>%s</td><td>%s</td>", performance.play().name(), performance.audience()));
+			result.append(String.format("<td>%s</td></tr>\n", usd(performance.amount())));
+		}
+		result.append("</table>\n");
+		result.append(String.format("<p>총액: <em>%s</em></p>\n", usd(data.totalAmount())));
+		result.append(String.format("<p>적립 포인트: <em>%s</em>점</p>\n", data.totalVolumeCredits()));
+
+		return result.toString();
 	}
 
 	private String renderPlainText(final StatementData data) {
