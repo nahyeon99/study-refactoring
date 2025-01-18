94f4684584867952e74608e66b3526b940450f82
nahyeon99 <nahyeonee99@gmail.com>
2025-01-18 23:39:48 +0900

    refactor: amountFor 파라미터 네이밍 변경

--- src/main/java/chapter1/Statement.java
+++ src/main/java/chapter1/Statement.java
@@ -44,21 +44,21 @@ public class Statement {
 		return result.toString();
 	}
 
-	public long amountFor(Performance performance, Play play) {
+	private long amountFor(Performance aPerformance, Play play) {
 		long result = 0;
 		switch (play.type()) {
 			case "tragedy":
 				result = 40_000;
-				if (performance.audience() > 30) {
-					result += 1_000 * (performance.audience() - 30);
+				if (aPerformance.audience() > 30) {
+					result += 1_000 * (aPerformance.audience() - 30);
 				}
 				break;
 			case "comedy":
 				result = 30_000;
-				if (performance.audience() > 20) {
-					result += 10_000 + 500 * (performance.audience() - 20);
+				if (aPerformance.audience() > 20) {
+					result += 10_000 + 500 * (aPerformance.audience() - 20);
 				}
-				result += 300 * performance.audience();
+				result += 300 * aPerformance.audience();
 				break;
 			default:
 				throw new IllegalArgumentException("unknown type: " + play.type());
