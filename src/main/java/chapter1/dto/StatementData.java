9ad0de832691214bc61290bcf4199cf8111c2896
nahyeon99 <nahyeonee99@gmail.com>
2025-01-19 02:15:36 +0900

    refactor: 총합 계산 메서드 위치 이동

--- src/main/java/chapter1/dto/StatementData.java
+++ src/main/java/chapter1/dto/StatementData.java
@@ -5,12 +5,34 @@ import java.util.Map;
 
 public record StatementData(
 	String customer,
-	List<EnrichPerformance> performances
+	List<EnrichPerformance> performances,
+	long totalAmount,
+	long totalVolumeCredits
 ) {
 	public static StatementData from(final Invoice invoice, final Map<String, Play> plays) {
+		final List<EnrichPerformance> performances = invoice.performances().stream()
+			.map(aPerformance -> EnrichPerformance.from(aPerformance, plays))
+			.toList();
+
 		return new StatementData(invoice.customer(),
-			invoice.performances().stream()
-				.map(aPerformance -> EnrichPerformance.from(aPerformance, plays))
-				.toList());
+			performances,
+			totalAmount(performances),
+			totalVolumeCredits(performances));
+	}
+
+	private static long totalAmount(final List<EnrichPerformance> performances) {
+		long result = 0;
+		for (var performance : performances) {
+			result += performance.amount();
+		}
+		return result;
+	}
+
+	private static long totalVolumeCredits(final List<EnrichPerformance> performances) {
+		long result = 0;
+		for (var performance : performances) {
+			result += performance.volumeCredits();
+		}
+		return result;
 	}
 }
