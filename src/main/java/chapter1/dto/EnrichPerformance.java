79a662b9546c3492d70d291374bf130384eb8d25
nahyeon99 <nahyeonee99@gmail.com>
2025-01-19 02:05:11 +0900

    refactor: volumeCredits 메서드 위치 이동

--- src/main/java/chapter1/dto/EnrichPerformance.java
+++ src/main/java/chapter1/dto/EnrichPerformance.java
@@ -6,14 +6,26 @@ public record EnrichPerformance(
 	String playID,
 	Integer audience,
 	Play play,
-	long amount
+	long amount,
+	long volumeCredits
 ) {
 	public static EnrichPerformance from(final Performance aPerformance, final Map<String, Play> plays) {
 		final Play play = playFor(aPerformance, plays);
 		return new EnrichPerformance(aPerformance.playID(),
 			aPerformance.audience(),
 			play,
-			amountFor(aPerformance, play));
+			amountFor(aPerformance, play),
+			volumeCreditsFor(aPerformance, play));
+	}
+
+	private static long volumeCreditsFor(final Performance aPerformance, final Play play) {
+		long result = 0;
+		result += Math.max(aPerformance.audience() - 30, 0);
+
+		if ("comedy".equals(play.type())) {
+			result += Math.floor(aPerformance.audience() / 5);
+		}
+		return result;
 	}
 
 	private static long amountFor(final Performance aPerformance, final Play play) {
