a676a62f2b70ba9678bdc70e5353227e848ed92d
nahyeon99 <nahyeonee99@gmail.com>
2025-02-26 02:30:53 +0900

    refactor: 함수 인라인하기 적용

--- src/main/java/chapter6/InlineFunction.java
+++ src/main/java/chapter6/InlineFunction.java
@@ -3,15 +3,12 @@ package chapter6;
 public class InlineFunction {
 
 	public int rating(Driver aDriver) {
-		return moreThanFiveLateDelivers(aDriver) ? 2 : 1;
-	}
-
-	private boolean moreThanFiveLateDelivers(Driver aDriver) {
-		return aDriver.numberOfLateDelivers() > 5;
+		return aDriver.numberOfLateDelivers() > 5 ? 2 : 1;
 	}
 
 	public static void main(String[] args) {
 		InlineFunction inlineFunction = new InlineFunction();
-		System.out.println(inlineFunction.rating(new Driver(3)));;
+		System.out.println(inlineFunction.rating(new Driver(3)));
+		;
 	}
 }
