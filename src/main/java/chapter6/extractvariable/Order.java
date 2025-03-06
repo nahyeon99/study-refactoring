abe938f6cf4ac29aec9817e9454c0fe2e6b51cda
nahyeon99 <nahyeonee99@gmail.com>
2025-03-06 17:32:27 +0900

    refactor: shipping 변수로 표현식 추출

--- src/main/java/chapter6/extractvariable/Order.java
+++ src/main/java/chapter6/extractvariable/Order.java
@@ -16,7 +16,7 @@ public class Order {
 		// 가격 = 기본 가격 - 수량 할인 + 배송비
 		final int basePrice = quantity * itemPrice;
 		final double quantityDiscount = Math.max(0, quantity - 500) * itemPrice * 0.05;
-		return basePrice - quantityDiscount +
-			Math.min(basePrice * 0.1, 100);
+		double shipping = Math.min(basePrice * 0.1, 100);
+		return basePrice - quantityDiscount + shipping;
 	}
 }
