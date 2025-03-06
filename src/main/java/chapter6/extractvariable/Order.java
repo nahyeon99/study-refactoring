449c485b6e40d14a69536e948ea1a31ac17c52d6
nahyeon99 <nahyeonee99@gmail.com>
2025-03-06 17:28:48 +0900

    feat: 예제 샘플 코드 추가

---
+++ src/main/java/chapter6/extractvariable/Order.java
@@ -0,0 +1,21 @@
+package chapter6.extractvariable;
+
+public class Order {
+	private int quantity;
+	private int itemPrice;
+
+	public int getQuantity() {
+		return quantity;
+	}
+
+	public int getItemPrice() {
+		return itemPrice;
+	}
+
+	public double calculatePrice() {
+		// 가격 = 기본 가격 - 수량 할인 + 배송비
+		return quantity * itemPrice -
+			Math.max(0, quantity - 500) * itemPrice * 0.05 +
+			Math.min(quantity * itemPrice * 0.1, 100);
+	}
+}
