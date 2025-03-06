48d023d185f71420142d8fd77215bd4ae9bd8f96
nahyeon99 <nahyeonee99@gmail.com>
2025-03-06 17:29:52 +0900

    refactor: basePrice 변수로 표현식 추출

--- src/main/java/chapter6/extractvariable/Order.java
+++ src/main/java/chapter6/extractvariable/Order.java
@@ -14,7 +14,8 @@ public class Order {
 
 	public double calculatePrice() {
 		// 가격 = 기본 가격 - 수량 할인 + 배송비
-		return quantity * itemPrice -
+		int basePrice = quantity * itemPrice;
+		return basePrice -
 			Math.max(0, quantity - 500) * itemPrice * 0.05 +
 			Math.min(quantity * itemPrice * 0.1, 100);
 	}
