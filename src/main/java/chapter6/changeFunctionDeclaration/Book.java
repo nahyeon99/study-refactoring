425aa65d5ec40f8562908aa03f1fd0b115f0200e
nahyeon99 <nahyeonee99@gmail.com>
2025-03-06 19:45:06 +0900

    refactor: 본문을 새로운 임시 함수로 추출

--- src/main/java/chapter6/changeFunctionDeclaration/Book.java
+++ src/main/java/chapter6/changeFunctionDeclaration/Book.java
@@ -8,6 +8,10 @@ public class Book {
 	private List<String> reservations = new ArrayList<>();
 
 	public void addReservation(String customer) {
+		this.zz_addReservation(customer);
+	}
+
+	public void zz_addReservation(String customer) {
 		this.reservations.add(customer);
 	}
 }
